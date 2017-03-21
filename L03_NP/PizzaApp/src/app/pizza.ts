f104f4cada272eb2065b34049700977ab471a70d
Marius Cristea <marius.cristea@irian.ro>
2017-03-21 16:04:20 +0200

    L03 - Angular2 NgPipies

---
+++ L03_NP/PizzaApp/src/app/pizza.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by marius on 15/03/2017.
+ */
+export interface Pizza {
+  name: string,
+  price: number,
+  weight: number,
+  ingredients: string,
+  canPurchase: boolean,
+  soldOut: boolean
+}
