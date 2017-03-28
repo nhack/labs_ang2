8f9b61d72db542082fbb3a95eb41891ba76d1638
Marius Cristea <marius.cristea@irian.ro>
2017-03-28 15:48:34 +0200

    L06 - Angular2 NgPut - Final

--- L06_NR/PizzaApp/src/app/pizza/service/pizza.service.ts
+++ L06_NR/PizzaApp/src/app/pizza/service/pizza.service.ts
@@ -1,10 +1,12 @@
 import {Pizza} from "../domain/pizza";
 import {OpaqueToken} from "@angular/core";
+import {Review} from "../domain/review";
 /**
  * Created by marius on 27/03/2017.
  */
-export interface IPizzaService{
+export interface IPizzaService {
   getPizzas(): Promise<Array<Pizza>>;
+  addReview(pizza: Pizza, review: Review): Promise<Pizza>;
 }
 
 export const PIZZA_SERVICE = new OpaqueToken('app.pizza.service');
